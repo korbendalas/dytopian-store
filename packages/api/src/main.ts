c370fe638bac7190ce688276873953fa2c02b315
dalibor <daliborpetric99@gmail.com>
2023-12-14 01:12:51 +0100

    finally update docker to work -2

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -8,7 +8,11 @@ import { AtGuard } from './common/guards/at.guard';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost', 'https://dystopian.daliborpetric.com'], // Allow requests from this origin
+    origin: [
+      'http://localhost',
+      'http://localhost',
+      'https://dystopian.daliborpetric.com',
+    ], // Allow requests from this origin
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
