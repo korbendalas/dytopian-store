8b799b44374c924da4a2c46c58720c891a80b921
dannycherry <dalibor@cherry.tv>
2023-12-19 18:19:03 +0100

    installing svega i svacega

--- oss/src/main.tsx
+++ oss/src/main.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App  from './App';
+import '@radix-ui/themes/styles.css';
 import './index.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import { Theme } from '@radix-ui/themes';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
         <Router>
-            <App/>
+            <Theme appearance="dark">
+                <App/>
+            </Theme>
         </Router>
     </React.StrictMode>
 );
